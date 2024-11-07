61646a31b1f792d2518a1d72905777df42fbc14a
Lisowez <lisowez@icloud.com>
2024-11-08 00:54:06 +0300

    feat: add dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
+config();
+const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();

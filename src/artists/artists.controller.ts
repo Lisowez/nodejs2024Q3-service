b403c725032fd9a281f74551d30092628e19881b
Lisowez <lisowez@icloud.com>
2024-11-08 00:24:40 +0300

    feat: feat artists api

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  HttpCode,
+} from '@nestjs/common';
 import { ArtistsService } from './artists.service';
+import { CreateArtistDto } from './dto/create-artist.dto';
 
-@Controller('artists')
+@Controller('artist')
 export class ArtistsController {
   constructor(private readonly artistsService: ArtistsService) {}
+
+  @Get()
+  findAll() {
+    return this.artistsService.findAll();
+  }
+
+  @Get(':id')
+  findArtist(@Param('id') id: string) {
+    return this.artistsService.findArtist(id);
+  }
+
+  @Post()
+  createArtist(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistsService.createArtist(createArtistDto);
+  }
+
+  @Put(':id')
+  updateArtist(
+    @Param('id') id: string,
+    @Body() updateArtistDto: CreateArtistDto,
+  ) {
+    return this.artistsService.updateArtist(id, updateArtistDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  deleteArtist(@Param('id') id: string) {
+    this.artistsService.deleteArtist(id);
+  }
 }
--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { artistsData } from '../dataBase/artists.data';
+import { IArtist } from '../dataBase/artists.data';
+import { v4, validate } from 'uuid';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { albumsData } from '../dataBase/albums.data';
+import { tracksData } from '../dataBase/tracks.data';
 
 @Injectable()
-export class ArtistsService {}
+export class ArtistsService {
+  private artists = artistsData;
+  private albums = albumsData;
+  private tracks = tracksData;
+
+  findAll(): IArtist[] {
+    return this.artists;
+  }
+
+  findArtist(id: string): IArtist {
+    const artist = this.artists.find((artist) => artist.id === id);
+    if (!validate(id)) {
+      throw new BadRequestException(`Artist with id ${id} is not valid.`);
+    }
+    if (!artist) {
+      throw new NotFoundException(`Artist with id ${id} not found.`);
+    }
+    return artist;
+  }
+
+  createArtist(createArtistDto: CreateArtistDto) {
+    const newArtist: IArtist = {
+      id: v4(),
+      ...createArtistDto,
+    };
+    this.artists.push(newArtist);
+    return newArtist;
+  }
+
+  updateArtist(id: string, updateArtistDto: CreateArtistDto) {
+    const artist = this.findArtist(id);
+    Object.assign(artist, updateArtistDto);
+    return artist;
+  }
+
+  deleteArtist(id: string) {
+    const track = this.tracks.find((track) => track.artistId === id);
+    const album = this.albums.find((album) => album.artistId === id);
+    if (track) {
+      track.artistId = null;
+    }
+    if (album) {
+      album.artistId = null;
+    }
+    const artist = this.findArtist(id);
+    this.artists = this.artists.filter((artist) => artist.id !== id);
+  }
+}
